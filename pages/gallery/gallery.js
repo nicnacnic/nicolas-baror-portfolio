ee2c0fcacd048e413c6ad9bca3d98e74805d1f96
nicnacnic <nicnacnicyt@gmail.com>
2024-05-05 00:08:55 -0400

    Added content

--- pages/gallery/gallery.js
+++ pages/gallery/gallery.js
@@ -1,38 +1,73 @@
 async function load(config) {
     try {
-        let container = document.querySelector('.gallery-container');
+        let container = document.querySelector('.gallery-pane-container');
 
-        for (let i = 0; i < config.gallery.length; i++) {
-            let div = document.createElement('div');
-            div.setAttribute('class', 'gallery-section-container');
-            let header = document.createElement('h2');
-            header.innerHTML = config.gallery[i].title;
-            let content = await generateGallery(config.gallery[i].content, config.gallery[i].title);
-            div.appendChild(header);
-            div.innerHTML += content;
-            container.appendChild(div);
+        let list = config.gallery.sort((a, b) => { return new Date(b.date).getTime() - new Date(a.date).getTime() });
+
+        let transitionDelay = 0;
+
+        for (let i = 0; i < list.length; i++) {
+            container.innerHTML += `<div class="gallery-image onClick="" name="" ${(i > 3) ? "hidden" : ""}" index="${i}">
+                    <img src="${list[i].image}" alt="${list[i].name}"></img>
+                <a class="hover-container" onClick="" href="${list[i].url}" data-link>
+                    <h3>${list[i].name}</h3>
+                    <h2><i>${list[i].role}</i></h2>
+                </a>
+            </div>`
+            transitionDelay += 250;
         }
+        // let div = document.createElement('div');
+        // div.setAttribute('class', 'gallery-section-container');
+        // let header = document.createElement('h2');
+        // header.innerHTML = 'test';
+        // let content = await generateGallery(config.gallery);
+        // div.appendChild(header);
+        // div.innerHTML += content;
+
+        let hiddenContainers = container.querySelectorAll('.gallery-image');
+        let index = 0;
+        let showInterval = setInterval(() => {
+            hiddenContainers[index].style.transition = 'opacity 500ms';
+            hiddenContainers[index].style.opacity = '100';
+            if (index >= hiddenContainers.length - 1) return clearInterval(showInterval);
+            index++;
+        }, 100)
     } catch { };
 }
 
-async function generateGallery(items, name) {
-    return new Promise((resolve) => {
-        let html = '';
-        for (let i = 0; i < items.length; i++) {
-            html += `<div class="gallery-image onClick="" name="${name}" ${(i > 3) ? "hidden" : ""}" index="${i}">
-                    <img src="${items[i].image}" alt="${items[i].name}"></img>
-                <a class="hover-container" onClick="" href="${items[i].url}" data-link>
-                    <h3>${items[i].name}</h3>
-                </a>
-            </div>`
-            if (i >= items.length - 1) return resolve(html);
-        }
-    })
-}
+async function sortGallery() {
+    let filterMethod = document.querySelector('.gallery-filter-select').options[document.querySelector('.gallery-filter-select').selectedIndex].value;
+    let sortMethod = document.querySelector('.gallery-sort-select').options[document.querySelector('.gallery-sort-select').selectedIndex].value;
+    let list = [];
+    if (sortMethod === 'date-ascending') list = config.gallery.sort((a, b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime()
+    });
+    else if (sortMethod === 'date-descending') list = config.gallery.sort((a, b) => {
+        return new Date(a.date).getTime() - new Date(b.date).getTime()
+    });
+    else if (sortMethod === 'show-ascending') list = config.gallery.sort((a, b) => { return a.name.localeCompare(b.name) });
+    else if (sortMethod === 'show-descending') list = config.gallery.sort((a, b) => { return b.name.localeCompare(a.name) });
 
-{/* <div class="gallery-image" index="0">
-    <img src="" alt=""></img>
-    <div class="hover-container">
-        <h3></h3>
-    </div>
-</div> */}
+    let container = document.querySelector('.gallery-pane-container');
+    container.innerHTML = '';
+
+    for (let i = 0; i < list.length; i++) {
+        if (filterMethod === 'all' || list[i].group === filterMethod)
+        container.innerHTML += `<div class="gallery-image onClick="" name="" ${(i > 3) ? "hidden" : ""}" index="${i}">
+                <img src="${list[i].image}" alt="${list[i].name}"></img>
+            <a class="hover-container" onClick="" href="${list[i].url}" data-link>
+                <h3>${list[i].name}</h3>
+                <h2><i>${list[i].role}</i></h2>
+            </a>
+        </div>`
+    }
+
+    let hiddenContainers = container.querySelectorAll('.gallery-image');
+    let index = 0;
+    let showInterval = setInterval(() => {
+        hiddenContainers[index].style.transition = 'opacity 500ms';
+        hiddenContainers[index].style.opacity = '100';
+        if (index >= hiddenContainers.length - 1) return clearInterval(showInterval);
+        index++;
+    }, 100)
+}
